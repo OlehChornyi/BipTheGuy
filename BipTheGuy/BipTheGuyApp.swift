5fd6b7850b3dac3ccc100b4133500a79aee5120c
oleh <phd.chornyi@gmail.com>
2025-01-16 17:29:24 +0200

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by Oleh on 16.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BipTheGuy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BipTheGuy
+//
+//  Created by Oleh on 16.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
